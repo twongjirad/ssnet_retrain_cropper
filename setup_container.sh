7a3cd8bbb0463260b7cf43d43c9ad78dac369f62
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2018-02-22 19:15:20 -0500

    save

---
+++ setup_container.sh
@@ -0,0 +1,4 @@
+source /usr/local/bin/thisroot.sh
+cd /usr/local/share/dllee_unified
+source configure.sh
+cd /cluster/kappa/90-days-archive/wongjiradlab/twongj01/ssnet_retrain_cropper/
---
+++ start_container.sh
@@ -0,0 +1,2 @@
+module load singularity
+singularity shell /cluster/kappa/90-days-archive/wongjiradlab/larbys/images/dllee_unified/singularity-dllee-intertool-20180216.img
